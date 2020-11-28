70197ca0316e5274d5ca5d8d05a7b3d949a5e42a
toriningenn <annamihailova72@yandex.com>
2020-11-28 12:08:48 +0100

    Queue practice

--- src/main/java/CarLinkedList.java
+++ src/main/java/CarLinkedList.java
@@ -1,6 +1,6 @@
 import java.util.Iterator;
 
-public class CarLinkedList implements CarList {
+public class CarLinkedList implements CarList,CarQueue {
 
     private Node first;
     private Node last;
@@ -11,6 +11,17 @@ public class CarLinkedList implements CarList {
         return getNode(index).value;
     }
 
+    @Override
+    public Car peek() {
+        return size > 0 ? get(0) : null;
+    }
+
+    @Override
+    public Car poll() {
+        Car car = get(0);
+        removeAt(0);
+        return car;
+    }
 
     @Override
     public boolean contains(Car car) {
